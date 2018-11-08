8ff15d39d03b465a652f949924cdef4ef02176d3
DESKTOP-O0K1JML\kosta <paskal1234@naver.com>
2018-11-08 14:41:54 +0900

    [쇼핑카트 dto 수정]

--- iceland/src/main/java/com/euishoe/carts/dao/MybatisCartDao.java
+++ iceland/src/main/java/com/euishoe/carts/dao/MybatisCartDao.java
@@ -1,6 +1,12 @@
 package com.euishoe.carts.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.euishoe.carts.dto.Cart;
 
 public class MybatisCartDao implements CartDao {
 	
@@ -14,6 +20,25 @@ public class MybatisCartDao implements CartDao {
 
 	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
 		this.sqlSessionFactory = sqlSessionFactory;
+	}
+
+	@Override
+	public List<Map<Integer, Cart>> listCart() {
+		Cart cart = null;
+		
+		List<Map<Integer,Cart>> list = new ArrayList<Map<Integer,Cart>>();
+		
+		return null;
+	}
+
+	@Override
+	public void createCart(Cart cart) {
+		
+	}
+
+	@Override
+	public void deleteCart(int cartNum) {
+		
 	}
 }
 
--- iceland/src/main/java/com/euishoe/carts/dto/Cart.java
+++ iceland/src/main/java/com/euishoe/carts/dto/Cart.java
@@ -1,9 +1,15 @@
 package com.euishoe.carts.dto;
 
+import com.euishoe.images.dto.Image;
+import com.euishoe.products.dto.Product;
+import com.euishoe.products.dto.ProductInfo;
+
 /**
  * 쇼핑몰 장바구니 dto
  * 
- * @author 전상일
+ * 사용되는 테이블 : carts,images,products,product_infos
+ * 
+ * @author 박호준
  *
  */
 public class Cart {
@@ -13,15 +19,25 @@ public class Cart {
 	private String productCode; 			// 상품코드
 	private String customerId; 				// 회원아이디
 	
+	// product_code
+	private Image image;
+	// product_code
+	private Product product;
+	// product_num
+	private ProductInfo productinfo;
 	
 	public Cart() {
 	}
 
-	public Cart(int cartNum, String productCode, String customerId) {
+	public Cart(int cartNum, String productCode, String customerId, Image image, Product product,
+			ProductInfo productinfo) {
 		super();
 		this.cartNum = cartNum;
 		this.productCode = productCode;
 		this.customerId = customerId;
+		this.image = image;
+		this.product = product;
+		this.productinfo = productinfo;
 	}
 
 	public int getCartNum() {
@@ -48,11 +64,36 @@ public class Cart {
 		this.customerId = customerId;
 	}
 
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public ProductInfo getProductinfo() {
+		return productinfo;
+	}
+
+	public void setProductinfo(ProductInfo productinfo) {
+		this.productinfo = productinfo;
+	}
 
 	@Override
 	public String toString() {
-		return "Cart [cartNum=" + cartNum + ", productCode=" + productCode + ", customerId=" + customerId
-				+ "]";
+		return "Cart [cartNum=" + cartNum + ", productCode=" + productCode + ", customerId=" + customerId + ", image="
+				+ image + ", product=" + product + ", productinfo=" + productinfo + "]";
 	}
+
+
 	
 }
